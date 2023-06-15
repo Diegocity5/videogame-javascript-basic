8ef63e7735fafd22c43df007103cd786360844a0
Diegocity5 <icotecc@gmail.com>
2023-06-15 18:30:26 -0500

    calculos de altura y ancho tanto de canvas como elementos internos

--- game.js
+++ game.js
@@ -5,13 +5,32 @@ const game = canvas.getContext("2d");
 window.addEventListener("load", startGame);
 
 function startGame(){
+    let canvasSize;
+
+    if(window.innerHeight > window.innerWidth){
+        canvasSize = window.innerWidth * 0.8;
+    }else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+    canvas.setAttribute("width", canvasSize);
+    canvas.setAttribute("height", canvasSize);
+
+    const elementsSize = canvasSize / 10;
+    console.log({canvasSize, elementsSize});
+    
+    game.font = elementsSize + "px Verdana"
+    game.textAling = "end";
+
+    for(let i=0; i<=10; i++){
+        game.fillText(emojis['X'], elementsSize * i, elementsSize);
+    }
     //Metodos y propiedades de estilo para retangulos
     // game.fillRect(0, 50, 100, 100);
     // game.clearRect(50, 50, 50, 50);
 
     //Metodos y propiedades de estilo para texto
-    game.font = "48px roboto";
-    game.fillStyle = "limegreen";
-    game.textAlign = "center";
-    game.fillText("Platzo", 10, 50);
+    // game.font = "48px roboto";
+    // game.fillStyle = "limegreen";
+    // game.textAlign = "center";
+    // game.fillText("Platzo", 10, 50);
 }

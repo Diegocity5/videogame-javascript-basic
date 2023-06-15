4c11e07fe85a24439174143cc9c59a5adfab0182
Diegocity5 <icotecc@gmail.com>
2023-06-14 23:06:10 -0500

    repasando propiedades y metodos del api canvas

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
-console.log(maps);
+//USE API CANVAS
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+
+window.addEventListener("load", startGame);
+
+function startGame(){
+    //Metodos y propiedades de estilo para retangulos
+    // game.fillRect(0, 50, 100, 100);
+    // game.clearRect(50, 50, 50, 50);
+
+    //Metodos y propiedades de estilo para texto
+    game.font = "48px roboto";
+    game.fillStyle = "limegreen";
+    game.textAlign = "center";
+    game.fillText("Platzo", 10, 50);
+}

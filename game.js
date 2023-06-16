f3973ddc541170078d8865a6da1f7ab0ed4b6c79
Diegocity5 <icotecc@gmail.com>
2023-06-15 23:17:52 -0500

    canvas responsive

--- game.js
+++ game.js
@@ -2,27 +2,34 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
-window.addEventListener("load", startGame);
+let canvasSize;//Size of canvas
+let elementsSize;//Size elements into canvas
 
-function startGame(){
-    let canvasSize;
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize(){
 
     if(window.innerHeight > window.innerWidth){
         canvasSize = window.innerWidth * 0.8;
     }else {
         canvasSize = window.innerHeight * 0.8;
     }
+
     canvas.setAttribute("width", canvasSize);
     canvas.setAttribute("height", canvasSize);
 
-    const elementsSize = canvasSize / 10;
-    console.log({canvasSize, elementsSize});
-    
+    elementsSize = canvasSize / 10;
+
+    startGame();
+}
+
+function startGame(){
     game.font = elementsSize + "px Verdana"
     game.textAling = "end";
 
     for(let i=0; i<=10; i++){
-        game.fillText(emojis['X'], elementsSize * i, elementsSize);
+        game.fillText(emojis['X'], elementsSize, elementsSize * i);
     }
     //Metodos y propiedades de estilo para retangulos
     // game.fillRect(0, 50, 100, 100);

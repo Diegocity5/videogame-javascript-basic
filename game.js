2a8270a2c4ce39e5cf538814b08df065b89acdeb
Diegocity5 <icotecc@gmail.com>
2023-06-21 18:18:57 -0500

    objet player position

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
 //USE API CANVAS
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
-
 const btnUp = document.querySelector("#up");
 const btnLeft = document.querySelector("#left");
 const btnRight = document.querySelector("#right");
@@ -9,6 +8,11 @@ const btnDown = document.querySelector("#down");
 
 let canvasSize; //Size of canvas
 let elementsSize; //Size elements into canvas
+
+const playerPosition = {
+  x: undefined,
+  y: undefined
+}
 
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
@@ -32,28 +36,40 @@ function startGame() {
   game.font = elementsSize + "px Verdana";
   game.textAlign = "end";
 
-  const map = maps[1];
-  let mapsRows = map.trim().split("\n");
-  let mapsCols = mapsRows.map((item) => item.trim().split(""));
+  const map = maps[0];
+  const mapsRows = map.trim().split("\n");
+  const mapsRowCols = mapsRows.map((item) => item.trim().split(""));
 
-  mapsCols.forEach((row, rowI) => {
+  mapsRowCols.forEach((row, rowI) => {
     row.forEach((col, colI) => {
       const emoji = emojis[col];
       const posX = elementsSize * (colI + 1);
       const posY = elementsSize * (rowI + 1);
 
+      if (col == 'O') {
+        playerPosition.x = posX;
+        playerPosition.y = posY;
+        console.log({playerPosition});
+      }
+      
       game.fillText(emoji, posX, posY);
     });
   });
+
+  movePlayer();
 }
 
-document.addEventListener("keyup", accion);
+function movePlayer(){
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+document.addEventListener("keyup", moveByKeys);
 btnUp.addEventListener("click", moveToUp);
 btnLeft.addEventListener("click", moveToLeft);
 btnRight.addEventListener("click", moveToRight);
 btnDown.addEventListener("click", moveToDown);
 
-function accion(event) {
+function moveByKeys(event) {
   switch (event.key) {
     case "ArrowUp":
         moveToUp();
@@ -72,13 +88,21 @@ function accion(event) {
 
 function moveToUp(){
   console.log("move up");
+  playerPosition.y -= elementsSize;
+  movePlayer();
 }
 function moveToDown(){
   console.log("move down");
+  playerPosition.y += elementsSize;
+  movePlayer();
 }
 function moveToLeft(){
   console.log("move left");
+  playerPosition.x -= elementsSize;
+  movePlayer();
 }
 function moveToRight(){
   console.log("move right");
+  playerPosition.x += elementsSize;
+  movePlayer();
 }

816a2b94bc0396a7ff29b6317fbb7bba32809ac2
Diegocity5 <icotecc@gmail.com>
2023-06-21 15:39:42 -0500

    Detectanto eventos de botones y teclas de cursor

--- game.js
+++ game.js
@@ -2,44 +2,83 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
-let canvasSize;//Size of canvas
-let elementsSize;//Size elements into canvas
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
 
-window.addEventListener('load', setCanvasSize);
-window.addEventListener('resize', setCanvasSize);
+let canvasSize; //Size of canvas
+let elementsSize; //Size elements into canvas
 
-function setCanvasSize(){
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 
-    if(window.innerHeight > window.innerWidth){
-        canvasSize = window.innerWidth * 0.8;
-    }else {
-        canvasSize = window.innerHeight * 0.8;
-    }
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
 
-    canvas.setAttribute("width", canvasSize);
-    canvas.setAttribute("height", canvasSize);
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
 
-    elementsSize = canvasSize / 10;
+  elementsSize = canvasSize / 10;
 
-    startGame();
+  startGame();
 }
 
-function startGame(){
-    game.font = elementsSize + "px Verdana"
-    game.textAlign = "end";
+function startGame() {
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "end";
 
-    const map = maps[2];
-    let mapsRows = map.trim().split('\n');
-    let mapsCols = mapsRows.map(item => item.trim().split(""));
+  const map = maps[1];
+  let mapsRows = map.trim().split("\n");
+  let mapsCols = mapsRows.map((item) => item.trim().split(""));
 
-    mapsCols.forEach((row, rowI) => {
-        row.forEach((col, colI) => {
-            const emoji = emojis[col];
-            const posX = elementsSize * (colI + 1);
-            const posY = elementsSize * (rowI + 1);
+  mapsCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
 
-            game.fillText(emoji, posX, posY );
-            console.log({row, rowI, col, colI});
-        })
+      game.fillText(emoji, posX, posY);
     });
+  });
+}
+
+document.addEventListener("keyup", accion);
+btnUp.addEventListener("click", moveToUp);
+btnLeft.addEventListener("click", moveToLeft);
+btnRight.addEventListener("click", moveToRight);
+btnDown.addEventListener("click", moveToDown);
+
+function accion(event) {
+  switch (event.key) {
+    case "ArrowUp":
+        moveToUp();
+      break;
+    case "ArrowDown":
+        moveToDown();
+      break;
+    case "ArrowLeft":
+        moveToLeft();
+      break;
+    case "ArrowRight":
+        moveToRight();
+      break;
+  }
+}
+
+function moveToUp(){
+  console.log("move up");
+}
+function moveToDown(){
+  console.log("move down");
+}
+function moveToLeft(){
+  console.log("move left");
+}
+function moveToRight(){
+  console.log("move right");
 }
